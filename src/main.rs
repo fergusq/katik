30423ba8dc3ff45526964a26e3bc9225af2ee21f
Iikka Hauhio <iikka.hauhio@helsinki.fi>
2020-02-24 15:22:15 +0200

    nya filer

---
+++ src/main.rs
@@ -0,0 +1,309 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+mod zrajm;
+use zrajm::*;
+
+extern crate regex;
+#[macro_use] extern crate lazy_static;
+
+use std::collections::{HashSet, BTreeSet};
+
+use cursive::Cursive;
+use cursive::traits::*;
+use cursive::views::{Dialog, LinearLayout, TextContent, TextView, EditView};
+use cursive::utils::markup::StyledString;
+use cursive::theme::{Style, Effect};
+
+use enumset::EnumSet;
+
+use clap::{App, Arg};
+
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate serde_derive;
+
+use rocket::State;
+use rocket::response::NamedFile;
+use rocket_contrib::json::Json;
+
+fn main() {
+    let matches = App::new("Katik")
+        .version("0.0.1")
+        .author("Iikka Hauhio <iikka.hauhio@helsinki.fi>")
+        .about("Smart Klingon dictionary")
+        .arg(Arg::with_name("dictionary")
+            .help("Zrajm Dictionary file path")
+            .required(true)
+        )
+        .arg(Arg::with_name("server")
+            .help("Create a HTTP server instead of TUI interface")
+            .short("-s")
+            .long("server")
+        )
+        .get_matches();
+    let dict = read_dictionary(matches.value_of("dictionary").unwrap());
+    if let Ok(dict) = dict {
+        println!("Loaded {} words.", dict.words.len());
+        if matches.is_present("server") {
+            server(dict);
+        } else {
+            tui(dict);
+        }
+    } else {
+        eprintln!("Failed to load dictionary.");
+    }
+}
+
+fn server(dict: ZrajmDictionary) {
+    rocket::ignite()
+        .manage(dict)
+        .mount("/", routes![server_root, server_complete]).launch();
+}
+
+#[get("/")]
+fn server_root() -> Result<NamedFile, std::io::Error> {
+    NamedFile::open("index.html")
+}
+
+#[get("/complete/<word>")]
+fn server_complete(word: String, dict: State<ZrajmDictionary>) -> Json<CompleteResponse> {
+    Json(completion_json(&*dict, word.as_str()))
+}
+
+#[derive(Serialize)]
+struct CompleteResponse {
+    parsed: HashSet<Vec<BTreeSet<ZrajmWord>>>,
+    suggestions: BTreeSet<ZrajmWord>,
+}
+
+fn completion_json(dict: &ZrajmDictionary, word: &str) -> CompleteResponse {
+    let grammar = grammar();
+    let mut parsed_words = HashSet::new();
+    let mut suggestion_words = BTreeSet::new();
+    for mut g in grammar {
+        let (ending, parsed, suggestions) = complete(&dict, &mut g.1, word);
+        //println!(" {} {}", ending, suggestions.len());
+        if !parsed.is_empty() && (ending.is_empty() || !suggestions.is_empty()) {
+            parsed_words.insert(parsed);
+        }
+        if ending.is_empty() {
+            continue
+        }
+        suggestion_words.extend(suggestions)
+    }
+    CompleteResponse {
+        parsed: parsed_words,
+        suggestions: suggestion_words,
+    }
+}
+
+fn tui(dict: ZrajmDictionary) {
+    let mut siv = Cursive::default();
+
+    let compl = TextContent::new("");
+    let compl_view = TextView::new_with_content(compl.clone()).fixed_width(50);
+    let input_view = EditView::new()
+        .on_edit(move|_, text: &str, _| {
+            let text = text.to_string();
+            if text.contains("!") {
+                compl.set_content(translation_string(&dict, &text[text.match_indices("!").last().unwrap().0+1..]))
+            } else {
+                let words: Vec<&str> = text.split(" ").collect();
+                let last_word = words.last().unwrap_or(&"");
+                compl.set_content(completion_string(&dict, last_word))
+            }
+        })
+        .fixed_width(50);
+
+    siv.add_layer(Dialog::around(LinearLayout::vertical().child(input_view).child(compl_view)));
+
+    siv.run();
+}
+
+fn translation_string(dict: &ZrajmDictionary, word: &str) -> StyledString {
+    let mut ans = StyledString::new();
+    let mut suggestions = dict.en_index.get(&word.to_string()).map_or(Vec::new(), |a| a.iter().collect());
+    suggestions.sort_by(|a, b| a.tlh.cmp(&b.tlh));
+    for s in suggestions.iter().take(20) {
+        ans.append_plain(format!("{} - {:?}: {}\n", s.tlh, s.pos, s.en.join(", ")).as_str());
+    }
+    ans
+}
+
+fn completion_string(dict: &ZrajmDictionary, word: &str) -> StyledString {
+    let grammar = grammar();
+    let mut ans = StyledString::new();
+    let mut already_listed = HashSet::new();
+    for mut g in grammar {
+        let (ending, _parsed, suggestions) = complete(&dict, &mut g.1, word);
+        //println!(" {} {}", ending, suggestions.len());
+        if ending.is_empty() {
+            continue
+        }
+        let suggestions = suggestions.difference(&already_listed).cloned().collect::<HashSet<_>>();
+        already_listed.extend(suggestions.clone());
+        let mut suggestions = suggestions.iter().collect::<Vec<_>>();
+        suggestions.sort_by(|a, b| a.tlh.cmp(&b.tlh));
+        if !suggestions.is_empty() {
+            ans.append_styled(format!("{}:\n", g.0).as_str(), Style { effects: EnumSet::from(Effect::Bold), color: Option::None });
+        }
+        for s in suggestions.iter().take(10) {
+            ans.append_plain(format!("{} - {:?}: {}\n", s.tlh, s.pos, s.en.join(", ")).as_str())
+        }
+    }
+    ans
+}
+
+fn complete(dict: &ZrajmDictionary, grammar: &mut Vec<(ZrajmPOS, bool)>, word: &str) -> (String, Vec<BTreeSet<ZrajmWord>>, HashSet<ZrajmWord>) {
+    let (ending, parsed, poses) = complete_pos(dict, Vec::new(), grammar, word);
+    //println!("{:?} {:?}", ending, poses.len());
+    let mut ans_parsed = Vec::new();
+    for (word, pos) in parsed {
+        ans_parsed.push(dict.pos_index.get(&pos).unwrap_or(&HashSet::new()).iter().filter(|w| w.tlh == word).cloned().collect());
+    }
+    let mut ans_words = HashSet::new();
+    for pos in poses {
+        for dword in dict.pos_index.get(&pos.0).map_or(Vec::new(), |a| a.iter().collect()) {
+            if dword.tlh.starts_with(ending.as_str()) {
+                ans_words.insert(dword.clone());
+            }
+        }
+        if pos.1 {
+            break
+        }
+    }
+    (ending, ans_parsed, ans_words)
+}
+
+fn complete_pos(dict: &ZrajmDictionary, mut parsed: Vec<(String, ZrajmPOS)>, grammar: &mut Vec<(ZrajmPOS, bool)>, word: &str) -> (String, Vec<(String, ZrajmPOS)>, Vec<(ZrajmPOS, bool)>) {
+    if grammar.is_empty() {
+        return (String::from(word), parsed, grammar.clone())
+    }
+    let mut dwords = Vec::new();
+    if let Some(index_words) = dict.pos_index.get(&grammar[0].0) {
+        dwords.extend(index_words);
+    }
+    dwords.sort_by(|a, b| b.tlh.trim_end_matches("-").len().cmp(&a.tlh.trim_end_matches("-").len()));
+    for dword in dwords {
+        let dword_ending = dword.tlh.trim_end_matches("-");
+        // Valitaan ahneesti
+        if word == dword_ending {
+            return (String::from(word), parsed, grammar.clone())
+        }
+        if word.starts_with(dword_ending) {
+            let t = if dword.tlh.ends_with("-") {
+                ""
+            } else {
+                "-"
+            };
+            let ending = format!("{}{}", t, &word[dword_ending.len()..]);
+            let pos = grammar.remove(0);
+            parsed.push((dword.tlh.clone(), pos.0));
+            return complete_pos(dict, parsed, grammar, &ending)
+        }
+    }
+    if grammar[0].1 {
+        return (String::from(word), parsed, grammar.clone())
+    }
+    let mut new_grammar = grammar.clone();
+    new_grammar.remove(0);
+    let (word2, parsed2, grammar2) = complete_pos(dict, parsed.clone(), &mut new_grammar, word);
+    if *word == word2 {
+        (String::from(word), parsed, grammar.clone())
+    } else {
+        (word2, parsed2, grammar2)
+    }
+}
+
+fn grammar() -> Vec<(&'static str, Vec<(ZrajmPOS, bool)>)> {
+    vec![
+        ("Noun track", vec![
+            (ZrajmPOS::Noun, true),
+            (ZrajmPOS::NounSuffix1, false),
+            (ZrajmPOS::NounSuffix2, false),
+            (ZrajmPOS::NounSuffix3, false),
+            (ZrajmPOS::NounSuffix4, false),
+            (ZrajmPOS::NounSuffix5, false),
+        ]),
+        ("Verb track", vec![
+            (ZrajmPOS::VerbPrefix, false),
+            (ZrajmPOS::Verb, true),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix1, false),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix2, false),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix3, false),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix4, false),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix5, false),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix6, false),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix7, false),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix8, false),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix9, false),
+            (ZrajmPOS::VerbSuffixRover, false),
+        ]),
+        ("Nominalized verb track", vec![
+            (ZrajmPOS::VerbPrefix, false),
+            (ZrajmPOS::Verb, true),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix1, false),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix2, false),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix3, false),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix4, false),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix5, false),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix6, false),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix7, false),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix8, false),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix9, true),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::NounSuffix1, false),
+            (ZrajmPOS::NounSuffix2, false),
+            (ZrajmPOS::NounSuffix3, false),
+            (ZrajmPOS::NounSuffix4, false),
+            (ZrajmPOS::NounSuffix5, false),
+        ]),
+        ("Adjective track", vec![
+            (ZrajmPOS::Verb, true),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::NounSuffix5, false),
+        ]),
+        ("Pronoun track", vec![
+            (ZrajmPOS::Pronoun, true),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix1, false),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix2, false),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix3, false),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix4, false),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix5, false),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix6, false),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix7, false),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix8, false),
+            (ZrajmPOS::VerbSuffixRover, false),
+            (ZrajmPOS::VerbSuffix9, true),
+            (ZrajmPOS::VerbSuffixRover, false),
+        ]),
+        ("Numerals", vec![(ZrajmPOS::Numeral, true)]),
+        ("Adverbials", vec![(ZrajmPOS::Adverbial, true)]),
+        ("Conjunctions", vec![(ZrajmPOS::Conjunction, true)]),
+    ]
+}
---
+++ src/zrajm.rs
@@ -0,0 +1,284 @@
+use std::hash::{Hash, Hasher};
+use std::collections::{HashMap, HashSet};
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+use regex::Regex;
+
+#[derive(Debug, Clone, Eq, Serialize)]
+pub struct ZrajmWord {
+    pub homonym: i8,
+    pub sense: i8,
+    pub subsense: i8,
+    pub tlh: String,
+    pub en: Vec<String>,
+    pub sv: Vec<String>,
+    pub pos: ZrajmPOS,
+    pub fields: HashMap<String, String>,
+    pub tag: Vec<String>,
+    pub data: Vec<String>,
+    pub id: String,
+}
+
+impl ZrajmWord {
+    pub fn new() -> ZrajmWord {
+        ZrajmWord {
+            homonym: 1,
+            sense: 1,
+            subsense: 1,
+            tlh: String::new(),
+            en: Vec::new(),
+            sv: Vec::new(),
+            pos: ZrajmPOS::Unknown,
+            fields: HashMap::new(),
+            tag: Vec::new(),
+            data: Vec::new(),
+            id: String::new(),
+        }
+    }
+
+    pub fn en_index(&self) -> Vec<String> {
+        get_index_words(&self.en)
+    }
+
+    pub fn sv_index(&self) -> Vec<String> {
+        get_index_words(&self.sv)
+    }
+}
+
+impl PartialEq for ZrajmWord {
+    fn eq(&self, other: &ZrajmWord) -> bool {
+        self.id == other.id
+    }
+}
+
+impl Hash for ZrajmWord {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.id.hash(state);
+    }
+}
+
+impl PartialOrd for ZrajmWord {
+    fn partial_cmp(&self, other: &ZrajmWord) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for ZrajmWord {
+    fn cmp(&self, other: &ZrajmWord) -> std::cmp::Ordering {
+        let a = (&self.tlh, self.homonym, self.sense, self.subsense);
+        let b = (&other.tlh, other.homonym, other.sense, other.subsense);
+        a.cmp(&b)
+    }
+}
+
+fn get_index_words(translations: &Vec<String>) -> Vec<String> {
+    let mut ans = Vec::new();
+    for translation in translations {
+        let mut stack: Vec<String> = Vec::new();
+        stack.push(String::new());
+        for ch in translation.chars() {
+            match ch {
+                '<' | '«' => {
+                    stack.push(String::new());
+                }
+                '>' | '»' => {
+                    if let Some(word) = stack.pop() {
+                        ans.push(word);
+                    }
+                }
+                _ => {
+                    for s in &mut stack {
+                        s.push(ch);
+                    }
+                }
+            }
+        }
+        ans.push(stack.pop().unwrap());
+    }
+    ans
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize)]
+pub enum ZrajmPOS {
+    Adverbial,
+    Conjunction,
+    Exclamation,
+    Name,
+    Noun,
+    NounSuffix1,
+    NounSuffix2,
+    NounSuffix3,
+    NounSuffix4,
+    NounSuffix5,
+    Numeral,
+    Pronoun,
+    QuestionWord,
+    Verb,
+    VerbPrefix,
+    VerbSuffix1,
+    VerbSuffix2,
+    VerbSuffix3,
+    VerbSuffix4,
+    VerbSuffix5,
+    VerbSuffix6,
+    VerbSuffix7,
+    VerbSuffix8,
+    VerbSuffix9,
+    VerbSuffixRover,
+    Unknown,
+}
+
+impl ZrajmPOS {
+    pub fn new(text: &str) -> ZrajmPOS {
+        match text {
+            "adverbial" => ZrajmPOS::Adverbial,
+            "conjunction" => ZrajmPOS::Conjunction,
+            "exclamation" => ZrajmPOS::Exclamation,
+            "name" => ZrajmPOS::Name,
+            "noun" => ZrajmPOS::Noun,
+            "noun suffix type 1" => ZrajmPOS::NounSuffix1,
+            "noun suffix type 2" => ZrajmPOS::NounSuffix2,
+            "noun suffix type 3" => ZrajmPOS::NounSuffix3,
+            "noun suffix type 4" => ZrajmPOS::NounSuffix4,
+            "noun suffix type 5" => ZrajmPOS::NounSuffix5,
+            "numeral" => ZrajmPOS::Numeral,
+            "pronoun" => ZrajmPOS::Pronoun,
+            "question word" => ZrajmPOS::QuestionWord,
+            "verb" => ZrajmPOS::Verb,
+            "verb prefix" => ZrajmPOS::VerbPrefix,
+            "verb suffix type 1" => ZrajmPOS::VerbSuffix1,
+            "verb suffix type 2" => ZrajmPOS::VerbSuffix2,
+            "verb suffix type 3" => ZrajmPOS::VerbSuffix3,
+            "verb suffix type 4" => ZrajmPOS::VerbSuffix4,
+            "verb suffix type 5" => ZrajmPOS::VerbSuffix5,
+            "verb suffix type 6" => ZrajmPOS::VerbSuffix6,
+            "verb suffix type 7" => ZrajmPOS::VerbSuffix7,
+            "verb suffix type 8" => ZrajmPOS::VerbSuffix8,
+            "verb suffix type 9" => ZrajmPOS::VerbSuffix9,
+            "verb suffix type rover" => ZrajmPOS::VerbSuffixRover,
+            _ => ZrajmPOS::Unknown,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct ZrajmDictionary {
+    pub words: Vec<ZrajmWord>,
+    pub tlh_index: HashMap<String, HashSet<ZrajmWord>>,
+    pub pos_index: HashMap<ZrajmPOS, HashSet<ZrajmWord>>,
+    pub en_index: HashMap<String, HashSet<ZrajmWord>>,
+    pub sv_index: HashMap<String, HashSet<ZrajmWord>>,
+}
+
+impl ZrajmDictionary {
+    pub fn new() -> ZrajmDictionary {
+        ZrajmDictionary {
+            words: Vec::new(),
+            tlh_index: HashMap::new(),
+            pos_index: HashMap::new(),
+            en_index: HashMap::new(),
+            sv_index: HashMap::new(),
+        }
+    }
+
+    fn push_word(&mut self, word: &ZrajmWord) {
+        self.words.push(word.clone());
+
+        if !self.tlh_index.contains_key(&word.tlh) {
+            self.tlh_index.insert(word.tlh.clone(), HashSet::new());
+        }
+        self.tlh_index.get_mut(&word.tlh).unwrap().insert(word.clone());
+
+        if !self.pos_index.contains_key(&word.pos) {
+            self.pos_index.insert(word.pos, HashSet::new());
+        }
+        self.pos_index.get_mut(&word.pos).unwrap().insert(word.clone());
+
+        for index_word in word.en_index() {
+            if !self.en_index.contains_key(&index_word) {
+                self.en_index.insert(index_word.clone(), HashSet::new());
+            }
+            self.en_index.get_mut(&index_word).unwrap().insert(word.clone());
+        }
+
+        for index_word in word.sv_index() {
+            if !self.sv_index.contains_key(&index_word) {
+                self.sv_index.insert(index_word.clone(), HashSet::new());
+            }
+            self.sv_index.get_mut(&index_word).unwrap().insert(word.clone());
+        }
+    }
+}
+
+pub fn read_dictionary(filename: &str) -> io::Result<ZrajmDictionary> {
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
+
+    let mut dict = ZrajmDictionary::new();
+    let mut data = false;
+    let mut word = ZrajmWord::new();
+    let mut prev_field = String::new();
+
+    for maybe_line in reader.lines() {
+        let line: String = maybe_line?;
+        if line == "== start-of-data ==" {
+            data = true
+        }
+        if line == "== end-of-data ==" {
+            break
+        }
+        else if !data || line.starts_with("==") {
+            continue
+        }
+        if line.is_empty() {
+            if word.id != "" {
+                dict.push_word(&word)
+            }
+            word = ZrajmWord::new()
+        }
+
+        let fields: Vec<_> = line.split("\t").collect();
+
+        if fields.len() < 2 {
+            continue
+        }
+
+        match fields[0] {
+            "tlh:" => parse_tlh(&mut word, &fields),
+            "pos:" => word.pos = ZrajmPOS::new(fields[1]),
+            "en:" => word.en = fields[1].split(", ").map(|s| s.to_string()).collect(),
+            "sv:" => word.sv = fields[1].split(", ").map(|s| s.to_string()).collect(),
+            "tag:" => word.tag = fields[1].split("; ").map(|s| s.to_string()).collect(),
+            "data:" => word.data = fields[1].split("; ").map(|s| s.to_string()).collect(),
+            "id:" => word.id = String::from(fields[1]),
+            "" => word.fields.get_mut(&prev_field).unwrap_or(&mut String::new()).push_str(line.as_str()),
+            _ => {
+                let name = String::from(fields[0]);
+                let value = String::from(fields[1]);
+                word.fields.insert(name.clone(), value);
+                prev_field = name
+            }
+        }
+    }
+
+    Ok(dict)
+}
+
+fn parse_tlh(word: &mut ZrajmWord, fields: &Vec<&str>) {
+    lazy_static!{
+        static ref RE: Regex = Regex::new(r"(?x)
+        (\[(?P<homonym>\d+)\]\s)?
+        \{(?P<word>.*)\}
+        (\s\[(?P<sense>\d+)?(\.(?P<subsense>\d+))?\])?").unwrap();
+    }
+    if !RE.is_match(fields[1]) {
+        eprintln!("{} is not tlh", fields[1]);
+        return
+    }
+    let caps = RE.captures(fields[1]).unwrap();
+    
+    word.tlh = String::from(&caps["word"]);
+    word.homonym = caps.name("homonym").map_or(1, |m| m.as_str().parse::<i8>().unwrap());
+    word.sense = caps.name("sense").map_or(1, |m| m.as_str().parse::<i8>().unwrap());
+    word.subsense = caps.name("subsense").map_or(1, |m| m.as_str().parse::<i8>().unwrap());
+}
